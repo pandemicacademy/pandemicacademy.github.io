c003eb5d93d392ea1b81b60a78884cd1fe37d31f
Jeremy Boggs <jeremy@virginia.edu>
2021-11-30 10:01:07 -0500

    New build.

--- docs/bibliography/files/1767/dnsfeed.js
+++
@@ -1,1 +0,0 @@
-dnsfeed({"country":"US","state":"NY","stateName":"New York","zipcode":"11209","timezone":"America/New_York","latitude":"40.62080","longitude":"-74.04260","city":"Brooklyn","continent":"NA"})
--- docs/bibliography/files/1768/js_U5uNG7srzqgSTMY4JGNIoTbvmb630oix9GLqGB_chrw.js
+++
@@ -1,5 +0,0 @@
-// serves as jsonp callback for ipify
-// @see ip-tracking in ama_analytics.libraries
-function getIp(json) {
-  gaDataLayer[0].ip_address = json.ip;
-};
